7a22bc7317f71d064dcf3ca3661e9cb643c5c736
roy-subhojeet <subhojeet60@gmail.com>
2023-06-20 13:24:28 -0400

    Time: 155 ms (48.39%), Space: 69.5 MB (58.75%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        return nums.size() > unordered_set<int>(nums.begin(), nums.end()).size();
+        unordered_set<int> seen;
+        for(auto num : nums)
+        {
+            if(seen.count(num))
+            {
+                return true;
+            }
+            seen.insert(num);
+        }
+        
+        return false;
 }
 };
