38bd17cebfa41de4da21a5040605f743e21c85db
roy-subhojeet <subhojeet60@gmail.com>
2023-06-22 16:29:09 -0400

    Time: 11 ms (83.82%), Space: 6 MB (19.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,21 +3,13 @@ public:
     bool isPalindrome(int x) {
         if(x < 0)
             return false;
-        vector<int> num;
-        while(x != 0)
+        int temp = x;
+        long long int revVal = 0;
+        while(temp != 0)
         {
-            num.push_back(x%10);
-            x = x/10;
+            revVal = revVal*10 + temp%10;
+            temp = temp/10;
         }
-        int size = num.size();
-        cout << size;
-        for(auto n : num)
-        {
-            if(n == num[size-1])
-                size--;
-            else
-                return false;
-        }
-    return true;
+        return revVal == x;
     }
 };
