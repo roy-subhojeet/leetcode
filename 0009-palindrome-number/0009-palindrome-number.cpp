45b0dfcb09c81a670fcfb31749cf6faf3ff6332c
roy-subhojeet <subhojeet60@gmail.com>
2023-06-22 16:30:04 -0400

    Time: 11 ms (83.82%), Space: 5.9 MB (44.58%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,7 +4,7 @@ public:
         if(x < 0)
             return false;
         int temp = x;
-        long long int revVal = 0;
+        long int revVal = 0;
         while(temp != 0)
         {
             revVal = revVal*10 + temp%10;
