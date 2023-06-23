1c7bbe722dfc2647c32310c94799aa833b00dbe2
roy-subhojeet <subhojeet60@gmail.com>
2023-06-23 14:13:12 -0400

    Time: 45 ms (42.77%), Space: 19.9 MB (68.36%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string> res;
         vector<vector<string>> ret;
         map<string, vector<string>> mp;
         for(auto &s: strs)
