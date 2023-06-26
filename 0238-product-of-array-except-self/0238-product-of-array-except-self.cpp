cd169350ce658f47edf440bc271db4fb6bae6fe5
roy-subhojeet <subhojeet60@gmail.com>
2023-06-26 15:58:40 -0400

    Time: 19 ms (96.22%), Space: 23.9 MB (96.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,24 +3,16 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
     int length = nums.size();
     std::vector<int> answer(length, 1);
+    int productFromLeft = 1;
+    int productFromRight = 1;
 
-    // answer[i] now contains the product of all numbers to the left
-    // For the element at index '0', there are no elements to the left,
-    // hence answer[0] = 1
-    for (int i = 1; i < length; i++) {
-        answer[i] = nums[i - 1] * answer[i - 1];
+    for(int i = 0; i < length; i++)
+    {
+        answer[i] *= productFromLeft;
+        productFromLeft *= nums[i];
+        answer[length-i-1] *= productFromRight;
+        productFromRight *= nums[length-i-1];
     }
-
-    // R contains the product of all numbers to the right
-    // We start with the rightmost element and work our way to the left
-    int R = 1;
-    for (int i = length - 1; i >= 0; i--) {
-        // For the index 'i', R contains the product of all numbers to the right. 
-        // We update R to include the product of R and the current number
-        answer[i] = answer[i] * R;
-        R *= nums[i];
-    }
-
     return answer;
     }
 };
