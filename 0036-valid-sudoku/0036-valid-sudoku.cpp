828b46ff9ad89648b7e203fe25b15462cf48200e
roy-subhojeet <subhojeet60@gmail.com>
2023-06-27 15:35:14 -0400

    Time: 55 ms (5.47%), Space: 26.1 MB (5.12%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_map<int, int>> rows(9);
-        vector<unordered_map<int, int>> columns(9);
-        vector<unordered_map<int, int>> boxes(9);
-
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                char c = board[i][j];
-                if(c != '.') {
-                    int n = (int)c;
-                    int box_index = (i / 3 ) * 3 + j / 3;
-
-                    // Keep the current cell value
-                    rows[i][n]++;
-                    columns[j][n]++;
-                    boxes[box_index][n]++;
-
-                    // Check if this value has been already seen before
-                    if(rows[i][n] > 1 || columns[j][n] > 1 || boxes[box_index][n] > 1)
-                        return false;
+        vector<unordered_map<char, int>> rowVec(9);
+        vector<unordered_map<char, int>> columnVec(9);
+        vector<unordered_map<char, int>> boxVec(9);
+        char ran = '99';
+        int cnt = 0;
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                if(board[i][j] == '.')
+                {
+                    ++ran;
+                    board[i][j] = ran;
                 }
+                columnVec[j].insert({board[i][j], 1});
+                rowVec[i].insert({board[i][j], 1});
+                boxVec[(i/3)*3 + ((j)/3)].insert({board[i][j], 1});
             }
+            if(rowVec[i].size() != 9)
+                return false;
+        }
+        for(auto &val: columnVec)
+        {
+            if (val.size() != 9)
+                return false; 
+        }
+        for(auto &val: boxVec)
+        {
+            if (val.size() != 9)
+                return false;     
         }
         return true;
     }
