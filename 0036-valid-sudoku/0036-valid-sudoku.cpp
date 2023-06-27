83781753d218be911ce6609f78ff64c4dcbf4a5c
roy-subhojeet <subhojeet60@gmail.com>
2023-06-27 15:38:55 -0400

    Time: 27 ms (49.45%), Space: 18 MB (62.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_map<int, int>> rows(9);
-        vector<unordered_map<int, int>> columns(9);
-        vector<unordered_map<int, int>> boxes(9);
+        //vector<unordered_map<int, int>> rows(9);
+        //vector<unordered_map<int, int>> columns(9);
+        //vector<unordered_map<int, int>> boxes(9);
+
+        int rows[9][9] = {0};
+        int columns[9][9] = {0};
+        int boxes[9][9] = {0};
 
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 char c = board[i][j];
                 if(c != '.') {
-                    int n = (int)c;
+                    int n = (int)(c - '0') - 1; // Subtract '1' to make it 0-indexed
                     int box_index = (i / 3 ) * 3 + j / 3;
 
                     // Keep the current cell value
