3d4521f796afb955e4d16ebc93c14298d9370413
roy-subhojeet <subhojeet60@gmail.com>
2023-06-29 00:03:52 -0400

    Time: 0 ms (100%), Space: 5.9 MB (74.58%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        uint32_t mask = 1;
+
+        uint32_t num = n;
+        int count = 0;
+        while(num != 0)
+        {
+            num = num & (num-1);
+            ++count;
+        }
+        return count;
+
+        /* Simple Solution by Masking each bit at a time 
         int counter = 0;
+        uint32_t mask = 1;
         for(int i=0; i<32; i++)
         {
             if((n&mask) != 0)
@@ -10,5 +21,6 @@ public:
             mask <<= 1;
         }
         return counter;
+        */
     }
 };
