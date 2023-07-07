f9cab708e19cdd063b5d92cb27e774f948be7ddd
roy-subhojeet <subhojeet60@gmail.com>
2023-07-06 20:54:50 -0500

    Time: 17 ms (87.25%), Space: 17.9 MB (88.45%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+
+        int missingNum = nums.size();
+        int i = 0;
+        for(auto &num:nums)
+        {
+            missingNum ^= i;
+            missingNum ^= num;
+            i++;
+        }
+        return missingNum;
+        /* Formula Method
         int total = nums.size();
-        total = 0.5*(total *(total + 1));
+        total = 0.5*(total *(total + 1)); 
         for (auto &num: nums)
         {
             total -= num;
         }
-        return total;
+        return total;  */
     }
 };
