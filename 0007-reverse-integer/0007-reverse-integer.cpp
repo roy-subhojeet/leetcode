d0432735ab133a6c7ce1252e1646cf6075ccf7c8
roy-subhojeet <subhojeet60@gmail.com>
2023-07-07 00:06:42 -0500

    Time: 3 ms (51.25%), Space: 5.8 MB (92.53%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        if(x >= pow(2, 31) || x <= pow(-2, 31))
+            return 0;
+        signed int num = x;
+        signed int res = 0;
+        while(num != 0)
+        {
+            if(res >= pow(2, 31)/10 || res <= pow(-2, 31)/10)
+                return 0;
+            res = res*10 + num%10;
+            num = num /10;
+        }
+        return res;
+        
+    }
+};
