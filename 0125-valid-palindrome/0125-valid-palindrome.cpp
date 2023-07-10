16319540c5807fb32990da5c904aa3ad5e46cebf
roy-subhojeet <subhojeet60@gmail.com>
2023-07-10 13:39:10 -0400

    Time: 11 ms (14.49%), Space: 7.6 MB (34.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-    bool res = true;
-    if(s.empty())
-        return true;
-    int j = 0;
-    for(int i = 0; i< s.length(); i++)
-    {
-        if((s[i] >= 'a' && s[i] <= 'z') ||
-           (s[i] >= 'A' && s[i] <= 'Z') ||
-            (s[i] >= '0' && s[i] <= '9'))
+        string res;
+        for(auto &c: s)
         {
-            s[j] = s[i];
-            s[j] = tolower(s[j]);
-            j++;
+            if(isalnum(c))
+                res += tolower(c);
         }
-    }
-    s= s.substr(0,j);
-    int end = s.length()-1;
-    for(int itr = 0; itr < s.length(); itr++)
-    {
-        if(s[itr] != s[end])
+        int head = 0;
+        int tail = res.size()-1;
+        bool flg = true;
+        while(tail >= 0)
         {
-            res = false;
-            break;
+            if(res[head] != res[tail])
+                return false;
+            ++head;
+            --tail;
         }
-        end--;
-    }
-    return res;
+        return flg;
     }
 };
