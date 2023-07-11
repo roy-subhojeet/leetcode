b2e2cf8b0c7327cd367bacf973027b33263f6ad1
roy-subhojeet <subhojeet60@gmail.com>
2023-07-11 17:11:37 -0400

    Time: 206 ms (57.82%), Space: 25.8 MB (60.59%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] > 0)
+            {
+                break;
+            }
+            if(i !=0 && nums[i] == nums[i-1])
+            {
+                continue;
+            }
+            int head = i+1;
+            int tail = nums.size() - 1;
+            while(head < tail)
+            {
+                int lasthead = head;
+                int lasttail = tail;
+                if(nums[i] + nums[head] + nums[tail] > 0)
+                    --tail;
+                else if(nums[i] + nums[head] + nums[tail] < 0)
+                    ++head;
+                else if(nums[i] + nums[head] + nums[tail] == 0)
+                {
+                    vector<int> res;
+                    res.insert(res.end(), {nums[i], nums[head], nums[tail]});
+                    result.push_back(res);
+                    // Avoid duplicates when moving pointers
+                    while(head < tail && nums[head] == nums[head + 1]) ++head;
+                    while(head < tail && nums[tail] == nums[tail - 1]) --tail;
+                    ++head;
+                    --tail;
+                }
+            }
+        }
+        return result;
+    }
+};
