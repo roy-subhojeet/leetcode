b3d83144f7a97bf96b070805d6a6c5a04bc9c0f6
roy-subhojeet <subhojeet60@gmail.com>
2023-07-20 13:47:54 -0400

    Time: 975 ms (30.89%), Space: 37.6 MB (31.02%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,7 +1,57 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        // No Sort Approach
+        vector<vector<int>> fRes;
+        set<vector<int>> uRes;
+        unordered_map<int, int> mp;
+        unordered_set<int> seen;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(seen.insert(nums[i]).second)
+            {
+                for(int j = i+1; j < nums.size(); j++)
+                {
+                        int complement = -nums[i] - nums[j];
+                        auto it = mp.find(complement);
+                        if(it != mp.end() && it->second == i)
+                        {
+                            vector<int> triplet = {nums[i], nums[j], complement};
+                            sort(triplet.begin(), triplet.end());
+                            uRes.insert(triplet);
+                        }
+                    mp[nums[j]] = i;
+                }
+            }
+        }
+        return vector<vector<int>> (uRes.begin(), uRes.end());
+
+        /* Hash Map Approach
         sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for(int i = 0; i < nums.size() && nums[i] <= 0; i++)
+        {
+            unordered_set<int> found;
+            if(i !=0 && nums[i] == nums[i-1])
+            {
+                continue;
+            }
+            for(int j = i+1; j < nums.size(); j++)
+            {
+                int complement = -nums[i] - nums[j];
+                if(found.count(complement))
+                {
+                    res.push_back({nums[i], nums[j], complement});
+                    while(j+1 < nums.size() && nums[j] == nums[j+1]) ++j;
+                }
+                found.insert(nums[j]);
+            }
+        }
+        return res;
+        /*
+
+        /* Two Pointer Approach
         vector<vector<int>> result;
         for(int i = 0; i < nums.size(); i++)
         {
@@ -34,6 +84,6 @@ public:
                 }
             }
         }
-        return result;
+        return result; */
     }
 };
