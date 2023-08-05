382e3a56ed5472d8ff8592824f68937a9c3bf640
roy-subhojeet <subhojeet60@gmail.com>
2023-08-05 00:20:52 -0400

    Time: 3 ms (77.5%), Space: 11.1 MB (13.28%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -10,18 +10,24 @@ public:
             {
                 return mid;
             }
+             // If the first half is sorted
             else if(nums[mid] >= nums[left])
             {
+                // Check if target lies in the first half
                 if(nums[mid] > target && nums[left] <= target)
                     right = mid - 1;
+                // Otherwise target lies in the second half
                 else
                     left = mid + 1;
             }
+            // If the second half is sorted
             else
             {
+                // Check if target lies in the second half
                 if(nums[mid] < target && nums[right] >= target)
                     left = mid + 1;
                 else
+                // Otherwise target lies in the first half
                     right = mid - 1;
             }
         }
