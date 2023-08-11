9f972f8de3e7742f354a40ec4cb49f61baa6c86b
roy-subhojeet <subhojeet60@gmail.com>
2023-08-11 01:29:39 -0400

    Time: 6 ms (75.75%), Space: 7.2 MB (60.94%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,6 +1,42 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+
+        if (s1.size() > s2.size()) {
+            return false;
+        }
+
+        vector<int> mpS1(26, 0), window(26, 0);
+
+        // Count the frequency of characters in s1
+        for (auto &s : s1) {
+            mpS1[s - 'a']++;
+        }
+
+        // Create a window of s1.size() in s2
+        int left = 0, right = 0;
+        while (right < s2.size()) {
+            char c = s2[right];
+            right++;
+            window[c - 'a']++;
+
+            // When the window size is equal to s1's size, check the counts
+            if (right - left == s1.size()) {
+                if (mpS1 == window) {
+                    return true;
+                }
+
+                // Slide the window to the right
+                char leftChar = s2[left];
+                left++;
+                window[leftChar - 'a']--;
+            }
+        }
+
+        return false;
+    }
+
+        /* Sliding Window
         if (s1.size() > s2.size()) {
             return false;
         }
@@ -37,6 +73,7 @@ public:
 
         return false;
     }
+*/
 
 /* Initial Solution
         if(s1.size() > s2.size())
