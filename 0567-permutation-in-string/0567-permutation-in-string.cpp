f862eb2d62013a7053638517104f9d7381a388ba
roy-subhojeet <subhojeet60@gmail.com>
2023-08-11 01:15:52 -0400

    Time: 11 ms (47.39%), Space: 8 MB (23.19%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) {
+            return false;
+        }
+
+        unordered_map<char, int> mpS1, window;
+
+        // Count the frequency of characters in s1
+        for (auto &s : s1) {
+            mpS1[s]++;
+        }
+
+        // Create a window of s1.size() in s2
+        int left = 0, right = 0;
+        while (right < s2.size()) {
+            char c = s2[right];
+            right++;
+            window[c]++;
+
+            // When the window size is equal to s1's size, check the counts
+            if (right - left == s1.size()) {
+                if (mpS1 == window) {
+                    return true;
+                }
+
+                // Slide the window to the right
+                char leftChar = s2[left];
+                left++;
+                window[leftChar]--;
+                if (window[leftChar] == 0) {
+                    window.erase(leftChar);
+                }
+            }
+        }
+
+        return false;
+    }
+};
