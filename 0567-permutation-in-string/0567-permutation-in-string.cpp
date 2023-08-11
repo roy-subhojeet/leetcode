5be6f39d02079d85adb02a664457fe926594f622
roy-subhojeet <subhojeet60@gmail.com>
2023-08-11 01:24:35 -0400

    Time: 11 ms (47.39%), Space: 8.1 MB (23.19%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -5,11 +5,11 @@ public:
             return false;
         }
 
-        vector<int> mpS1(26, 0), window(26, 0);
+        unordered_map<char, int> mpS1, window;
 
         // Count the frequency of characters in s1
         for (auto &s : s1) {
-            mpS1[s - 'a']++;
+            mpS1[s]++;
         }
 
         // Create a window of s1.size() in s2
@@ -17,7 +17,7 @@ public:
         while (right < s2.size()) {
             char c = s2[right];
             right++;
-            window[c - 'a']++;
+            window[c]++;
 
             // When the window size is equal to s1's size, check the counts
             if (right - left == s1.size()) {
@@ -28,10 +28,43 @@ public:
                 // Slide the window to the right
                 char leftChar = s2[left];
                 left++;
-                window[leftChar - 'a']--;
+                window[leftChar]--;
+                if (window[leftChar] == 0) {
+                    window.erase(leftChar);
+                }
             }
         }
 
         return false;
     }
+
+/* Initial Solution
+        if(s1.size() > s2.size())
+            return false;
+        
+        unordered_map<char, int> mpS1;
+        for(auto &s : s1)
+        {
+            mpS1[s]++;
+        }
+
+        int j = 0;
+        for(int i = 0; i < s2.length(); i++)
+        {
+            auto itr = mpS1.find(s2[i]);
+            auto cpMap = mpS1;
+            for(int k = i; k < s2.size() && cpMap.size() != 0; k++)
+            {
+                if(cpMap.find(s2[k]) == cpMap.end())
+                {
+                    break;
+                }
+                --cpMap[s2[k]];
+                if(cpMap[s2[k]] == 0) cpMap.erase(s2[k]);
+                if(cpMap.size() == 0) return true;
+            }
+        }
+        return false;
+    }
+*/
 };
