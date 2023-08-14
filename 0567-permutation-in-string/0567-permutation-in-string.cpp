adfcc2da0c4d45ae0a98a4b9fadcd8a3648bc8fb
roy-subhojeet <subhojeet60@gmail.com>
2023-08-14 03:47:33 -0400

    Time: 9 ms (57.66%), Space: 7.3 MB (60.92%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        vector<int> s1Bucket(26, 0);
+        vector<int> window(26, 0);
 
-        // optimised sliding window
-        if (s1.size() > s2.size()) {
-            return false;
+        for(auto &s : s1)
+        {
+            s1Bucket[s - 'a']++;
         }
-
-        vector<int> mpS1(26, 0), window(26, 0);
-
-        // Count the frequency of characters in s1
-        for (auto &s : s1) {
-            mpS1[s - 'a']++;
-        }
-
-        // Create a window of s1.size() in s2
         int left = 0, right = 0;
-        while (right < s2.size()) {
-            char c = s2[right];
-            right++;
-            window[c - 'a']++;
-
-            // When the window size is equal to s1's size, check the counts
-            if (right - left == s1.size()) {
-                if (mpS1 == window) {
-                    return true;
-                }
-
-                // Slide the window to the right
-                char leftChar = s2[left];
-                left++;
-                window[leftChar - 'a']--;
+        while(right < s2.size())
+        {
+            window[s2[right] - 'a']++;
+            ++right;
+            if(right - left == s1.size())
+            {
+                if(window == s1Bucket) return true;
+                char c = s2[left];
+                --window[c - 'a'];
+                ++left;
             }
         }
-
         return false;
     }
 
