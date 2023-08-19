4709caf0a9f69e77376043be4ef259df9f90262a
roy-subhojeet <subhojeet60@gmail.com>
2023-08-19 16:24:52 -0400

    Time: 142 ms (83.72%), Space: 102.9 MB (53.47%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,6 +3,18 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
 
         stack<int> st;
+        vector<int> res(temperatures.size(), 0);
+
+        for (int i = 0; i < temperatures.size(); ++i) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                res[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return res;
+        /* initial attempt redundant checks
+        stack<int> st;
         vector<int> res(temperatures.size(),0);
 
         for(int i = 0; i < temperatures.size(); ++i)
@@ -27,6 +39,7 @@ public:
             }
         }
         return res;
+        */
 
         /* O(n^2) Sliding window approach
         int left = 0;
