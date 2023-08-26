cc6ad0843d75c44aea11f2733c4668711fbcef60
roy-subhojeet <subhojeet60@gmail.com>
2023-08-25 21:54:47 -0400

    Time: 311 ms (65.28%), Space: 128.5 MB (75.99%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
@@ -1,31 +1,37 @@
 class TimeMap {
-
 private:
-unordered_map<string, vector<pair<int, string>>> mp;
+
+unordered_map<string, vector<pair<string, int>>> mp;
+
 public:
     TimeMap() {
+        
     }
     
     void set(string key, string value, int timestamp) {
 
-        mp[key].push_back({timestamp, value});
+        mp[key].push_back({value, timestamp});
         
     }
     
     string get(string key, int timestamp) {
+
         if(mp.find(key) == mp.end()) return "";
-        if(timestamp < mp[key][0].first) return "";
+
+        if(timestamp < mp[key][0].second) return "";
+
         int left = 0;
         int right = mp[key].size() - 1;
-        int s = right;
         while(left <= right)
         {
             int mid = left - (left - right)/2;
-            if(mp[key][mid].first == timestamp) return mp[key][mid].second;
-            else if(mp[key][mid].first > timestamp) right = mid - 1;
+            if(mp[key][mid].second > timestamp) right = mid - 1;
             else left = mid + 1;
         }
-        return mp[key][right].second;
+
+        return mp[key][right].first;
+
+        
     }
 };
 
