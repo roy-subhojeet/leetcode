e49531a84131d56acb165ff4931ad9c8aacf080a
roy-subhojeet <subhojeet60@gmail.com>
2023-09-27 02:31:34 -0400

    Time: 0 ms (100%), Space: 6.4 MB (11.71%) - LeetHub

---
+++ 0916-decoded-string-at-index/0916-decoded-string-at-index.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+
+        string a;
+        long long cnt = 0;
+        int N = s.size();
+        for(auto & c : s)
+        {
+            if(c > 96 and c < 123)
+            {
+                cnt++;
+            }
+            else
+            {
+                int i = c - '0';
+                cnt = cnt*i;
+            }
+        }
+        for (int i = N-1; i >=0; --i) {
+            k %= cnt;
+            if (k == 0 && isalpha(s[i]))
+                return (string) "" + s[i];
+
+            if (isdigit(s[i]))
+                cnt /= s[i] - '0';
+            else
+                cnt--;
+        }
+        return "";
+
+    }
+};
