59a1679c1dcaab17b904cde5c3ada1f7d95de79e
roy-subhojeet <subhojeet60@gmail.com>
2023-10-01 15:30:22 -0400

    Time: 12 ms (76.92%), Space: 18.4 MB (7.69%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
+        if(nums.size() < 3) return 0;
         int n = nums.size();
-        long long ans = 0;
-
+        vector<int> maxArray(n, INT_MIN);
+        vector<int> maxDiff(n, INT_MAX);
         vector<int> maxRight(n, INT_MIN);
-        for (int i = n - 2; i >= 0; --i) {
-            maxRight[i] = max(maxRight[i + 1], nums[i + 1]);
+        maxArray[0] = nums[0];
+        for(int i = 1 ; i < n; ++i)
+        {
+            maxArray[i] = max(nums[i], maxArray[i-1]);
+        }
+        maxDiff[0] = maxArray[0] - nums[0];
+        int maxDif = INT_MIN;
+        int pos = 0;
+        for(int i = 1 ; i < n; ++i)
+        {
+            maxDiff[i] = max(maxArray[i] - nums[i], maxDiff[i-1]);
+        }
+        maxRight[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; --i)
+        {
+            maxRight[i] = max(nums[i], maxRight[i+1]);
         }
 
-        for (int i = 0; i < n - 2; ++i) {
-            for (int j = i + 1; j < n - 1; ++j) {
-                if (nums[i] > nums[j]) {
-                    ans = max<long long int>(ans, (long long)(nums[i] - nums[j]) * maxRight[j]);
-                }
-            }
+        long long ans = LLONG_MIN;
+        for(int i = 0; i < nums.size()-1; ++i)
+        {
+            ans = max((long long)maxDiff[i] * maxRight[i+1], ans);
         }
+        
+        return ans>0 ? ans: 0;
 
-        return ans;
+
+        
     }
 };
