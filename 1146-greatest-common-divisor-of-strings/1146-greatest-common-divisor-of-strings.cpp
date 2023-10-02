3ce42f737d90785abe28977ebada0a5e14912deb
roy-subhojeet <subhojeet60@gmail.com>
2023-10-02 02:51:08 -0400

    Time: 5 ms (47.84%), Space: 7.6 MB (18.46%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -6,6 +6,6 @@ public:
             return "";
         }
         int gcdLength = gcd(str1.size(), str2.size());
-        return str1.substr(0, gcdLength);
+        return str2.substr(0, gcdLength);
     }
 };
