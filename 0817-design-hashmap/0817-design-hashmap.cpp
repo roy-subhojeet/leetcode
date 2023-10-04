1e6178f8f12aa2bb8d7970009c0af27896d9bc2b
roy-subhojeet <subhojeet60@gmail.com>
2023-10-04 19:16:29 -0400

    Time: 95 ms (92.12%), Space: 54.6 MB (60.88%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+#include <list>
+
+template<typename U, typename V>
+class Pair {
+public:
+    U first;
+    V second;
+
+    Pair(U first, V second) : first(first), second(second) {}
+};
+
+class Bucket {
+private:
+    std::list<Pair<int, int>> bucket;
+
+public:
+    Bucket() {}
+
+    int get(int key) {
+        for (auto& pair : bucket) {
+            if (pair.first == key) {
+                return pair.second;
+            }
+        }
+        return -1;
+    }
+
+    void update(int key, int value) {
+        bool found = false;
+        for (auto& pair : bucket) {
+            if (pair.first == key) {
+                pair.second = value;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            bucket.emplace_back(key, value);
+        }
+    }
+
+    void remove(int key) {
+        for (auto it = bucket.begin(); it != bucket.end(); ++it) {
+            if (it->first == key) {
+                bucket.erase(it);
+                break;
+            }
+        }
+    }
+};
+
+class MyHashMap {
+private:
+    int key_space;
+    std::vector<Bucket> hash_table;
+
+public:
+    MyHashMap() : key_space(2069), hash_table(key_space) {}
+
+    void put(int key, int value) {
+        int hash_key = key % key_space;
+        hash_table[hash_key].update(key, value);
+    }
+
+    int get(int key) {
+        int hash_key = key % key_space;
+        return hash_table[hash_key].get(key);
+    }
+
+    void remove(int key) {
+        int hash_key = key % key_space;
+        hash_table[hash_key].remove(key);
+    }
+};
+
+// Usage:
+// MyHashMap obj;
+// obj.put(key, value);
+// int param_2 = obj.get(key);
+// obj.remove(key);
