501ef36b825f9fbded89e15c7e4e2b82a0fbc291
roy-subhojeet <subhojeet60@gmail.com>
2023-10-05 03:11:10 -0400

    Time: 7 ms (87.9%), Space: 16.2 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,50 +1,26 @@
-#include <vector>
-
 class Solution {
 public:
-    std::vector<int> majorityElement(std::vector<int>& nums) {
-
-        int count1 = 0;
-        int count2 = 0;
-
-        int candidate1 = 0; 
-        int candidate2 = 0; 
-        bool candidate1Exists = false;
-        bool candidate2Exists = false; 
-
-        for (int n : nums) {
-            if (candidate1Exists && candidate1 == n) {
-                count1++;
-            } else if (candidate2Exists && candidate2 == n) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = n;
-                count1++;
-                candidate1Exists = true;
-            } else if (count2 == 0) {
-                candidate2 = n;
-                count2++;
-                candidate2Exists = true;
-            } else {
-                count1--;
-                count2--;
-            }
-        }
-
-        std::vector<int> result;
-
-        count1 = 0;
-        count2 = 0;
-
-        for (int n : nums) {
-            if (candidate1Exists && n == candidate1) count1++;
-            if (candidate2Exists && n == candidate2) count2++;
-        }
-
-        int n = nums.size();
-        if (count1 > n/3) result.push_back(candidate1);
-        if (count2 > n/3) result.push_back(candidate2);
-
-        return result;
-    }
+  vector<int> majorityElement(vector<int> &a) 
+  {
+	  int candidateA(-1), candidateB(-1), countA(0), countB(0);
+    
+	  for (const auto & elem: a) 
+	  {
+		  if (elem == candidateA) countA++;
+		  else if (elem == candidateB) countB++;
+		  else if (! countA) candidateA = elem, countA = 1;
+		  else if (! countB) candidateB = elem, countB = 1;
+		  else countA--, countB--;
+     }
+      
+     countA = countB = 0;
+     for (const auto & elem: a)
+		 if (elem == candidateA) countA++;
+		 else if (elem == candidateB) countB++;
+  
+	  vector<int> r;
+	  if (countA > size(a)/3) r.push_back(candidateA);
+	  if (countB > size(a)/3) r.push_back(candidateB);
+	  return r;
+  }
 };
