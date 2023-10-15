07458bd89c61a45781febbbc26a167463ec0ac83
roy-subhojeet <subhojeet60@gmail.com>
2023-10-15 19:39:36 -0400

    Time: 0 ms (100%), Space: 6.6 MB (16.76%) - LeetHub

--- 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
-int minOperations(long n) {
-    int one = __builtin_popcountl(n);
-    if (one <= 1) return one;
-    return 1 + min(minOperations(n+(n&(-n))), minOperations(n-(n&(-n))));
+int minOperations(int n){
+	int power = log2(n);
+	if((1 << power) == n)	return 1;
+	int diff1 = minOperations(n-(1 << power));
+	int diff2 = minOperations((1 << (power + 1))-n);
+	return 1 + min(diff1, diff2);
 }
 };
