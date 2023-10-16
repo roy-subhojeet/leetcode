06592cf3107a7165c75c16dfac582740ab12623f
roy-subhojeet <subhojeet60@gmail.com>
2023-10-15 20:58:32 -0400

    Time: 7 ms (11.58%), Space: 6.3 MB (37.47%) - LeetHub

--- 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int minOperations(long n) {
-        int one = __builtin_popcountl(n);
-        if (one <= 1) return one;
-        return 1 + min(minOperations(n+(n&(-n))), minOperations(n-(n&(-n))));
+        cout << n << endl;
+        int bitCountOne = __builtin_popcountl(n);
+        if(bitCountOne == 1) return bitCountOne;
+        return 1+min(minOperations(n+(n&(-n))), minOperations(n-(n&(-n))));
     }
 };
