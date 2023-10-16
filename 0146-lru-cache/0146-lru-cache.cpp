b0ab6b266ccf1a100ff3f5112b9cd07cf217322f
roy-subhojeet <subhojeet60@gmail.com>
2023-10-16 16:27:52 -0400

    Time: 385 ms (60.97%), Space: 176.9 MB (38.05%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,122 +1,88 @@
-class LRUCache {
-private:
+#include <unordered_map>
 
-    /* Data Structures, Private member variables */
+class LRUCache
+{
+public:
     struct ListNode
     {
-        ListNode * next;
-        ListNode * prev;
-        int val;
+        ListNode* next;
+        ListNode* prev;
         int key;
-        ListNode(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr){}
+        int val;
+        ListNode(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr) {}
     };
 
-    ListNode* head = new ListNode(-1, -1);
-    ListNode* tail = new ListNode(-1, -1);
-    int capacity;
-    unordered_map<int, ListNode*> mp;
-
-    /* Methods*/
-
-    void addNode(ListNode *node)
+    LRUCache(int capacity)
     {
-        ListNode *secondLast = tail->prev;
-        secondLast->next = node;
-        node->next = tail;
-        node->prev = secondLast;
-        tail->prev = node;
+        head->next = tail;
+        tail->prev = head;
+        this->capacity = capacity;
     }
 
-    void removeNode(ListNode *node)
+    ~LRUCache() {
+        while (head) {
+            ListNode* next = head->next;
+            delete head;
+            head = next;
+        }
+    }
+
+    int get(int key)
+    {
+        if(mp.find(key) == mp.end()) return -1;
+        detachNode(mp[key]);  // Detach but don't delete
+        addNode(mp[key]);
+        return mp[key]->val;
+    }
+
+    void put(int key, int value)
+    {
+        if(mp.find(key) != mp.end())
+        {
+            detachNode(mp[key]);
+            deleteNode(mp[key]);  // Delete after detaching
+            mp.erase(key);
+        }
+        
+        ListNode *newNode = new ListNode(key, value);
+        addNode(newNode);
+        mp[key] = newNode;
+
+        if(mp.size() > capacity)
+        {
+            ListNode *toRemove = head->next;
+            detachNode(toRemove); 
+            mp.erase(toRemove->key);
+            deleteNode(toRemove);  // Delete after detaching
+        }
+    }
+
+private:
+
+    int capacity;
+    std::unordered_map<int, ListNode*> mp;
+    ListNode* head = new ListNode(-1, -1);
+    ListNode* tail = new ListNode(-1, -1);
+
+    void addNode(ListNode* node)
+    {
+        ListNode *last = tail->prev;
+        last->next = node;
+        node->next = tail;
+        node->prev = last;
+        tail->prev = node;   
+    }
+
+    // Detaches the node from the list but doesn't delete it
+    void detachNode(ListNode* node)
     {
         node->prev->next = node->next;
         node->next->prev = node->prev;
     }
 
-public:
-
-    LRUCache(int capacity) {
-        this->capacity = capacity;
-        head->next = tail;
-        tail->prev = head;
-    }
-
-    ~LRUCache() {
-        ListNode* current = head;
-        while (current) {
-            ListNode* next = current->next;
-            delete current;
-            current = next;
-        }
-    }
-    
-    int get(int key) {
-
-        if(mp.find(key) == mp.end()) return -1;
-        removeNode(mp[key]);
-        addNode(mp[key]);
-        return mp[key]->val;
-    }
-    
-    void put(int key, int value) {
-        if(mp.find(key) != mp.end())
-        {
-            removeNode(mp[key]);
-            delete mp[key];  // Don't forget to deallocate the memory
-        }
-        mp[key] = new ListNode(key, value);
-        addNode(mp[key]);
-        if(mp.size() > capacity)
-        {
-            ListNode *toDelete = head->next;
-            removeNode(toDelete);
-            mp.erase(toDelete->key);
-            delete toDelete;  // free the memory
-        }
+    // Deletes the node
+    void deleteNode(ListNode* node)
+    {
+        delete node;
     }
 };
-
-
-/* My Sol 14/22 - also correcting it would give TLE
-
-class LRUCache {
-
-private:
-unordered_map<int, int> p;
-queue<int> st;
-int capacity;
-public:
-    LRUCache(int capacity) {
-        this->capacity = capacity;
-        
-    }
-    
-    int get(int key) {
-        if(p.find(key) == p.end()) return -1;
-        if(st.front() == key) st.pop();
-        st.push(key);
-        return p[key];
-    }
-    
-    void put(int key, int value) {
-        //cout << p.size() << " "<< capacity<< endl;
-        p[key] = value;
-        if(p.size() > capacity && !st.empty()) 
-        {
-            int i = st.front();
-            p.erase(i);
-            st.pop();
-            //return;
-        }
-        if(st.front() == key) st.pop();
-        st.push(key);
-        
-    }
-};
-*/
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
