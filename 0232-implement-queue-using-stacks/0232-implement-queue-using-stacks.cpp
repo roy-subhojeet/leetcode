90474ac33bdfc0427f32bcfc7ea721776b85dc41
roy-subhojeet <subhojeet60@gmail.com>
2023-10-16 23:25:40 -0400

    Time: 0 ms (100%), Space: 7.6 MB (5.86%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -6,19 +6,14 @@ public:
 
     stack<int> st1;
     stack<int> st2;
+    int top;
     
     void push(int x) {
+        if(st1.empty()) top = x;
         st1.push(x);
     }
     
     int pop() {
-        peek();
-        int j = st2.top();
-        st2.pop();
-        return j;
-    }
-    
-    int peek() {
         if(st2.empty())
         {       
             while(!st1.empty())
@@ -27,6 +22,13 @@ public:
                 st1.pop();
             }
         }
+        int j = st2.top();
+        st2.pop();
+        return j;
+    }
+    
+    int peek() {
+        if(st2.empty()) return top;
         return st2.top();
     }
     
