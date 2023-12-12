f268a4a8cb179e6b6ce8ae1c636934adfaf52bef
roy-subhojeet <subhojeet60@gmail.com>
2023-12-11 21:25:46 -0500

    Time: 24 ms (88.35%), Space: 13.5 MB (49.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,5 +1,53 @@
 class Solution {
 public:
+    int numIslands(vector<vector<char>>& grid) {
+        /* Using BFS */
+        queue<pair<int, int>> neighbours;
+        int row = grid.size();
+        int ans = 0;
+        int column = grid[0].size();
+        for(int i = 0; i < row; ++i)
+        {
+            for(int j = 0; j < column; ++j)
+            {
+                if(grid[i][j] == '1') 
+                {
+                    neighbours.push({i,j});
+                    grid[i][j] = '0';
+                    ++ans;
+                }
+                while(!neighbours.empty())
+                {
+                    auto val = neighbours.front();
+                    neighbours.pop();
+                    int r = val.first;
+                    int c = val.second;
+                    if(r-1 >= 0 && grid[r-1][c] == '1') 
+                    {
+                        neighbours.push({r-1, c});
+                        grid[r-1][c] = '0';
+                    }
+                    if(r+1 < row && grid[r+1][c] == '1') 
+                    {
+                        neighbours.push({r+1, c});
+                        grid[r+1][c] = '0';
+                    }
+                    if(c-1 >= 0 && grid[r][c-1] == '1')
+                    {
+                         neighbours.push({r, c-1});
+                         grid[r][c-1] = '0';
+                    }
+                    if(c+1 < column && grid[r][c+1] == '1') 
+                    {
+                        neighbours.push({r, c+1});
+                        grid[r][c+1] = '0';
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+/* DFS 
     void dfs(vector<vector<char>> &grid, int row, int column)
     {
         grid[row][column] = '0';
@@ -25,4 +73,5 @@ public:
         }
         return ans;
     }
+*/
 };
