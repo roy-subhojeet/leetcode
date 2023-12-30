0916ee412a52b3715835d88cbf1c7c2c3e7687cb
roy-subhojeet <subhojeet60@gmail.com>
2023-12-30 15:51:33 -0500

    Time: 20 ms (29.68%), Space: 13.2 MB (49.68%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,33 +1,51 @@
 class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
+        // A tree must have exactly n - 1 edges (where n is the number of nodes).
+        // If not, it can't be a valid tree.
         if(edges.size() != n-1) return false;
+
+        // Adjacency matrix to represent the graph.
         vector<vector<int>> adjMat(n, vector<int>());
+        // A map to track the parent of each node.
         map<int, int> parentMap;
 
-        for(int i = 0 ; i < edges.size(); ++i)
-        {
+        // Construct the graph: for each edge, add an entry to the adjacency matrix
+        // in both directions (since it's an undirected graph).
+        for(int i = 0; i < edges.size(); ++i) {
             adjMat[edges[i][0]].push_back(edges[i][1]);   
             adjMat[edges[i][1]].push_back(edges[i][0]);   
         }
+
+        // Stack for DFS traversal.
         stack<int> st;
+        // Start with node 0 and mark its parent as -1 (no parent).
         st.push(0);
         parentMap[0] = -1;
-        while(!st.empty())
-        {
+
+        // DFS traversal.
+        while(!st.empty()) {
             auto s = st.top();
             st.pop();
-            for(auto & e : adjMat[s])
-            {
+
+            // Check the neighbors of the current node.
+            for(auto & e : adjMat[s]) {
+                // If the neighbor is the parent of the current node, skip it.
                 if(parentMap[s] == e) continue;
+
+                // If the neighbor has already been discovered and it's not the parent,
+                // it means we have found a cycle. Thus, it's not a valid tree.
                 if(parentMap.find(e) != parentMap.end()) return false;
+
+                // Mark the current node as the parent of the neighbor and
+                // push the neighbor onto the stack for further exploration.
                 parentMap[e] = s;
                 st.push(e);
             }
         }
-        //for(auto v : visited) if(v == false) return false;
 
+        // After DFS, if we have discovered all nodes (the parent map has n entries),
+        // and haven't found any cycles, then it's a valid tree.
         return parentMap.size() == n;
-
     }
 };
